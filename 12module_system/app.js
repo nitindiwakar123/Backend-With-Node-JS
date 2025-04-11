7d288fc6b61b6e40b52f1e5247655d06132641b7
Nitin Diwakar <nitindiwakar2006@gmail.com>
2025-04-11 13:33:20 +0530

    add 12module_system

---
+++ 12module_system/app.js
@@ -0,0 +1,21 @@
+// What is a Module in Node.js?
+// A module is just a file. In Node.js, every JavaScript file is treated as a separate module. It helps in organizing code, avoiding polluting the global namespace, and reusing functionality.
+
+// There are two types of module systems in node js
+// 1. CommonJS (CJS) – Default Module System
+// 2. ES Modules (ESM) – Modern Module System
+
+// when we run this file in node js variable num treated as local variable but in browser it treated as global variable.
+
+// because Node.js is designed for modularity. Every file is treated as a module, and it wraps your code in a function behind the scene like this:
+
+// (function(exports, require, module, __filename, __dirname) {
+//     // your code here
+//   })();
+  
+var num = 0;
+console.log(num);
+
+// If you want a truly global variable in Node.js, you have to explicitly attach it to the global object:
+global.a = 10;
+console.log(global.a); 
