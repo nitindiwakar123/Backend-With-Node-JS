231f3d1c97bf730bbddb66502b62a7d3eb038dc1
Nitin Diwakar <nitindiwakar2006@gmail.com>
2025-04-17 20:11:43 +0530

    add 20module_types

---
+++ 20module_types/notes.js
@@ -0,0 +1,33 @@
+// ==========================================
+// Types of Modules in Node.js (Theory Only)
+// ==========================================
+
+/*
+1. Built-in / Core / Native Modules
+---------------------
+- These modules are provided by Node.js itself.
+- No need to install them separately.
+- Common examples include: fs, http, path, os, etc.
+- They are used for core functionalities like file system operations, creating servers, etc.
+*/
+
+
+/*
+2. Local Modules
+------------------
+- These are custom modules created by the developer.
+- They help in organizing code into reusable components.
+- You can create your own files and export functions, objects, or classes from them.
+- These modules are imported using a relative path (e.g., './math.js').
+*/
+
+
+/*
+3. Third-party Modules
+------------------------
+- These are external packages developed by the community.
+- They are installed using npm (Node Package Manager).
+- Provide extended functionality like web frameworks, color styling, utility functions, etc.
+- Examples: express, chalk, mongoose, lodash, etc.
+*/
+
